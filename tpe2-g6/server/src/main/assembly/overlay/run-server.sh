09347b54076d77ca8b698ba7f5b2bd67fd43a5e3
Patient Zero <crojas@itba.edu.ar>
2021-10-28 13:27:26 -0300

    Fixed null pointer exception on Server by giving it default ip value

--- tpe2-g6/server/src/main/assembly/overlay/run-server.sh
+++ tpe2-g6/server/src/main/assembly/overlay/run-server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-java  -cp 'lib/jars/*' "ar.edu.itba.pod.server.Server" $*
+java "$@" -cp 'lib/jars/*' "ar.edu.itba.pod.server.Server"
 
--- tpe2-g6/server/src/main/java/ar/edu/itba/pod/server/Server.java
+++ tpe2-g6/server/src/main/java/ar/edu/itba/pod/server/Server.java
@@ -8,6 +8,8 @@ import com.hazelcast.core.Hazelcast;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Collections;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.Properties;
 
 public class Server {
@@ -16,10 +18,16 @@ public class Server {
     public static void main(String[] args) throws IOException {
         logger.info("tpe2-g6 Server Starting ...");
 
-        Properties props = new Properties();
-        String path = Thread.currentThread().getContextClassLoader().getResource("").getPath() + "config.properties";
-        props.load(new FileInputStream(path));
-        String ip = props.getProperty("ip");
+        String ip = "192.168.1.*";
+        try {
+            Properties props = new Properties();
+            String path = Optional.ofNullable(Thread.currentThread().getContextClassLoader().getResource("")).orElseThrow(IllegalArgumentException::new).getPath() + "config.properties";
+            props.load(new FileInputStream(path));
+            ip = props.getProperty("ip");
+        } catch (IllegalArgumentException ignored) {
+
+        }
+        logger.info("Using ip: " + ip);
 
         // Config
         Config config = new Config();
